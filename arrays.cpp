c78032bb86b93889c67dcbac92b3c318c6a7e391
Nurillo Abdikodirov <59973492+NAbdikodirov@users.noreply.github.com>
2022-02-13 19:45:48 -0600

    Create arrays.cpp

---
+++ arrays.cpp
@@ -0,0 +1,25 @@
+//Nurillo Abdikodirov
+//CS-255 C++
+//Elmhurst University
+
+#include <iostream>
+using namespace std;
+
+/* Introduction to the Arrays in C++, and I am gpoing to write small and simple lines of code 
+* to demonstrate the meaning of Arrays.
+*/
+
+int main(){
+  int itemCounts[3];
+  
+  itemCounts[0] = 155;
+  itemCounts[1] = 119;
+  itemCounts[2] = 125;
+  
+  cout << itemCounts[1];
+  
+  return 0;
+}
+
+
+//
