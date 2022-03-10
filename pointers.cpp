f0b8d44c13598b72b0b917646d4ca7e75b8bd9be
Nurillo Abdikodirov <59973492+NAbdikodirov@users.noreply.github.com>
2022-03-10 14:02:36 -0600

    Update pointers.cpp

--- pointers.cpp
+++ pointers.cpp
@@ -47,3 +47,31 @@ int main() {
 
    return 0;
 }
+
+
+//How to find char in C string
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+   char personName[100];
+   char searchChar;
+   char* searchResult = nullptr;
+
+   cin.getline(personName, 100);
+   cin >> searchChar;
+
+   /* Your solution goes here  */
+   searchResult = strchr(personName, searchChar);
+
+   if (searchResult != nullptr) {
+      cout << "Character found." << endl;
+   }
+   else {
+      cout << "Character not found." << endl;
+   }
+
+   return 0;
+}
