475ae99a89015dfc874e3d13d00313298a4d1712
Nurillo Abdikodirov <59973492+NAbdikodirov@users.noreply.github.com>
2022-03-10 10:35:20 -0600

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,49 @@
+//this is a solid example for the Pointers in C++
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Car {
+   public:
+      Car(int distanceToSet = 0);
+      int GetDistanceTraveled() const;
+   private:
+      int distanceTraveled;
+};
+
+Car::Car(int distanceToSet) {
+   distanceTraveled = distanceToSet;
+}
+
+int Car::GetDistanceTraveled() const {
+   return distanceTraveled;
+}
+
+int GetTotalMiles(vector<Car*> carsList) {
+   int totalMiles;
+   unsigned int i;
+
+   totalMiles = 0;
+
+   for (i = 0; i < carsList.size(); i++) {
+      totalMiles = totalMiles + carsList.at(i)->GetDistanceTraveled();
+   }
+
+   return totalMiles;
+}
+
+int main() {
+   vector<Car*> garage;
+   garage.push_back(new Car(30));
+   garage.push_back(new Car(20));
+   garage.push_back(new Car(5));
+
+   cout << "Traveled: " << GetTotalMiles(garage) << endl;
+   delete garage.at(1);
+   garage.erase(garage.begin() + 1);
+   cout << "Traveled: " << GetTotalMiles(garage) << endl;
+
+   return 0;
+}
